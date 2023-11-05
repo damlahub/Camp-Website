7e75b6728d42bfbf755aec9efd3ce89d9d4759eb
damlaoksel <damlaaoksel@gmail.com>
2023-11-05 22:59:45 +0300

    flex-slider bölümü bitti

--- apps/flex-slide.js
+++ apps/flex-slide.js
@@ -1,49 +1,104 @@
 const accommodationArr = [
     {
-        image: "assets/headerbg.jpg",
-        title: "HeaderBg",
-        price: "200TL"
+        image: "assets/flex-slide/1.jpg",
+        title: "Günübirlik Giriş",
+        price: "100TL"
+
+
+    },
+    {
+        image: "assets/flex-slide/2.jpg",
+        title: "Kendi Çadırınızla Konaklama",
+        price: "250TL"
+
+    },
+    {
+        image: "assets/flex-slide/3.jpg",
+        title: "Tesis Çadır Konaklama",
+        price: "750TL"
+    },
+    {
+        image: "assets/flex-slide/4.jpg",
+        title: "Karavan Park",
+        price: "300TL"
+    },
+    {
+        image: "assets/flex-slide/5.jpg",
+        title: "Bungalov",
+        price: "1750TL"
+    },
+    {
+        image: "assets/flex-slide/6.jpg",
+        title: "Hobbit Ev",
+        price: "2000TL"
     }
 ];
 
 const accommodation = document.querySelector("#accommodation");
 
 const createContent = () => {
-    let flexSlide = document.createElement("section");
-    flexSlide.classList.add("accommodation-flex-slide");
-    flexSlide.innerHTML = `
-    <article class="accommodation-flex-slide-item">
-    <img src="${accommodationArr[0].image}" alt="">
-    <h1>${accommodationArr[0].title}</h1>
-    </article>
-    `;
-    accommodation.prepend(flexSlide);
+    accommodationArr.forEach((arr, index) => {
+        let flexSlide = document.createElement("section");
+        flexSlide.classList.add("accommodation-flex-slide");
+
+        flexSlide.innerHTML = `
+        <article class="accommodation-flex-slide-item">
+        <img src="${accommodationArr[index].image}" alt="">
+        <h1>${accommodationArr[index].title}</h1>
+        </article>
+        `;
+        accommodation.append(flexSlide);
+    });
 }
 
 const flexSlideActive = () => {
+    let flexButton = document.createElement("button");
     let flexPrice = document.createElement("p");
+    let flexInfo = document.createElement("article");
+    let activeFlexItem = null;
 
     let flexItems = document.querySelectorAll(".accommodation-flex-slide-item");
 
     flexItems.forEach((flexItem, index) => {
+        let animationInProgress = false;
+
         flexItem.addEventListener("click", () => {
+            if (animationInProgress) {
+                return; 
+            }
+            if (activeFlexItem && activeFlexItem !== flexItem) {
+                activeFlexItem.classList.remove("flex-slide-item-active");
+                flexPrice.remove();
+            }
+
             flexItem.classList.toggle("flex-slide-item-active");
 
             if (flexItem.classList.contains("flex-slide-item-active")) {
                 flexPrice.innerHTML = accommodationArr[index].price;
+                flexInfo.classList.add("flex-slide-info");
 
+                flexButton.classList.add("btn-standart");
+                flexButton.textContent = "Rezervasyon";
+
+                flexInfo.appendChild(flexPrice);
+                flexInfo.appendChild(flexButton);
                 setTimeout(() => {
-                    flexItem.appendChild(flexPrice);
+                    flexItem.appendChild(flexInfo);
+                    animationInProgress = false;
                 }, 400);
             } else {
-                flexPrice.remove();
+                flexInfo.remove();
+                animationInProgress = false;
+            }
+
+            if (flexItem.classList.contains("flex-slide-item-active")) {
+                activeFlexItem = flexItem;
+            } else {
+                activeFlexItem = null;
             }
         });
     });
 }
-
-// <button class="btn-standart">Rez</button>
-
 createContent();
 flexSlideActive();
 
